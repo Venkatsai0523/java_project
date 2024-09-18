7b268f5d97944b180d21eb14be18f4ea429c9815
Venkatsai0523 <aryasai64@gmail.com>
2024-09-18 14:51:14 +0530

    java_project

---
+++ src/main/java/org/yourcompany/yourproject/Java123.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Admin
+ */
+public class Java123 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
